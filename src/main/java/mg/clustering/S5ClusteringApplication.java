86a987f9fe9013529dbb803c36e47271df73b3a5
mendrika261 <mendrika261@icloud.com>
2023-11-12 15:18:38 +0300

    Initial commit

---
+++ src/main/java/mg/clustering/S5ClusteringApplication.java
@@ -0,0 +1,13 @@
+package mg.clustering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S5ClusteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S5ClusteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mg/clustering/S5ClusteringApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.clustering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S5ClusteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
